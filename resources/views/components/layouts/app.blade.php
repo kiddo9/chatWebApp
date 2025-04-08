556c92cd19f54b9d7745c2f44df4c7013e7a7472
kiddo9 <dkido913@gmail.com>
2025-04-08 15:55:05 +0100

    50th commit

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         @livewireStyles
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
     </head>
     <body>
         <x-chat-Navbar />
