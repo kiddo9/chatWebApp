94e09ab0f64774780aeadce8d4fef8d4f242b56f
kiddo9 <dkido913@gmail.com>
2025-04-08 14:42:48 +0100

    47th commit

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ $title ?? 'chatWebApp' }}</title>
         <link rel="stylesheet" href="{{ asset("build/assets/app-CaEiG72_.css")}}">
-        {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         @livewireStyles
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <x-chat-Navbar />
